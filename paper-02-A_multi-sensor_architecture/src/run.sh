479a39638b6314e63a5667c30d4b7f9c0991a974
Navar <navarmn@gmail.com>
2019-08-26 11:48:42 -0300

    fixed ignore

--- paper-02-A_multi-sensor_architecture/src/run.sh
+++
@@ -1,8 +0,0 @@
-echo 'Activating env'
-source ./.venv/bin/activate
-echo "Env is:"
-which python
-echo 'Appended experiment'
-python run_experiment_appended.py
-echo 'Union experiment'
-python run_experiment_union.py
